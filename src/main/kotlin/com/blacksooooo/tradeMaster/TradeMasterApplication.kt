9be59484360da4bfa5ff412858104683bc6fb6af
blackSooooo <blacksooooo@naver.com>
2024-08-10 21:46:13 +0900

    initial commit

---
+++ src/main/kotlin/com/blacksooooo/tradeMaster/TradeMasterApplication.kt
@@ -0,0 +1,11 @@
+package com.blacksooooo.tradeMaster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradeMasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<TradeMasterApplication>(*args)
+}
